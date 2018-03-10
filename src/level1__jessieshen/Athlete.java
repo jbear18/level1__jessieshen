aef401ca862890b7e052183e1d336f11979f2209
jbear18 <league@WTS-iMac-11.attlocal.net>
2018-03-09 16:59:57 -0800

    Athlete, Cuteness TV
    Athlete, Cuteness TV(IP)

--- src/level1__jessieshen/Athlete.java
+++ src/level1__jessieshen/Athlete.java
@@ -1,5 +1,38 @@
 package level1__jessieshen;
 
 public class Athlete {
+	// static means just 1
+	String name;
+	int speed;
+	static int runnerCount;
+	int shirtNumber;
+	String location= "Del Mar fairgrounds";
+	static int raceTime;
+	// constructor
+
+	public Athlete(String name, int speed) {
+		this.name = name;
+		this.speed = speed;
+		runnerCount++;
+		shirtNumber=runnerCount;
+	
+	}
+
+	public static void main(String[] args) {
+		Athlete sean = new Athlete("Sean", 1000);
+		Athlete john = new Athlete("John", 1002);
+		Athlete jessie = new Athlete("Jessie", 1000000);
+		System.out.println(sean.name + " has shirt number " + sean.shirtNumber);
+		System.out.println(john.name + " has shirt number " + john.shirtNumber);
+		System.out.println(jessie.name + " has shirt number " + jessie.shirtNumber);
+		System.out.println("There are " + Athlete.runnerCount + " people in the race");
+		System.out.println(sean.name+ " is running at "+ sean.location);
+		System.out.println(john.name+ " is running at "+ john.location);
+		System.out.println(jessie.name+ " is running at "+ jessie.location);
+		sean.location= "Penguintown";
+		System.out.println(sean.name+ " is running at "+ sean.location);
+		System.out.println(john.name+ " is running at "+ john.location);
+		System.out.println(jessie.name+ " is running at "+ jessie.location);
+	}
 
 }
---
+++ src/level1__jessieshen/CutenessTV.java
@@ -0,0 +1,13 @@
+package level1__jessieshen;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CutenessTV {
+JFrame frame= new JFrame("Cuteness TV");
+JPanel panel = new JPanel();
+JButton a= new JButton("Ink in Motion");
+JButton b = new JButton("blob");
+JButton c = new JButton("The Duck Song");
+}
